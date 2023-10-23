3728ec8399c2e180d9fc4c7b91478aeaca189bbe
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-23 09:54:48 +0530

    checkpoint isLoading

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,18 +1,29 @@
 "use client";
 
+import { MessagesContext } from "@/context/messages";
 import { cn } from "@/lib/utils";
 import { Message } from "@/lib/validators/message";
 import { useMutation } from "@tanstack/react-query";
+import { CornerDownLeft, Loader2 } from "lucide-react";
 import { nanoid } from "nanoid";
-import React, { FC, HTMLAttributes, useState } from "react";
+import React, { FC, HTMLAttributes, useContext, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 interface ChatInptuProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ChatInput: FC<ChatInptuProps> = ({ className, ...props }) => {
   const [input, setInput] = useState<string>("");
+  const {
+    messages,
+    addMessage,
+    removeMessage,
+    updateMessage,
+    setIsMessageUpdating,
+  } = useContext(MessagesContext);
 
-  const { mutate: sendMessage } = useMutation({
+  const textareaRef = useRef<null | HTMLTextAreaElement>(null);
+
+  const { mutate: sendMessage, isLoading } = useMutation({
     mutationFn: async (message: Message) => {
       const response = await fetch("/api/message", {
         method: "POST",
@@ -24,10 +35,24 @@ const ChatInput: FC<ChatInptuProps> = ({ className, ...props }) => {
 
       return response.body;
     },
+    onMutate(message) {
+      addMessage(message);
+    },
     onSuccess: async (stream) => {
       // console.log("Success");
       // getting readable from the server
       if (!stream) throw new Error("No stream found");
+
+      const id = nanoid();
+      const responseMessage: Message = {
+        id,
+        isUserMessage: false,
+        text: "",
+      };
+
+      addMessage(responseMessage);
+
+      setIsMessageUpdating(true);
 
       const reader = stream.getReader();
       const decoder = new TextDecoder();
@@ -38,8 +63,16 @@ const ChatInput: FC<ChatInptuProps> = ({ className, ...props }) => {
         const { value, done: doneReading } = await reader.read();
         done = doneReading;
         const chunkValue = decoder.decode(value);
-        console.log(chunkValue);
+        updateMessage(id, (prev) => prev + chunkValue);
       }
+
+      // cleaning up
+      setIsMessageUpdating(false);
+      setInput("");
+
+      setTimeout(() => {
+        textareaRef.current?.focus();
+      }, 10);
     },
   });
 
@@ -47,6 +80,7 @@ const ChatInput: FC<ChatInptuProps> = ({ className, ...props }) => {
     <div {...props} className={cn("border-t border-zinc-300", className)}>
       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
         <TextareaAutosize
+          ref={textareaRef}
           rows={2}
           onKeyDown={(e) => {
             if (e.key === "Enter" && !e.shiftKey) {
@@ -62,11 +96,27 @@ const ChatInput: FC<ChatInptuProps> = ({ className, ...props }) => {
             }
           }}
           maxRows={4}
+          disabled={isLoading}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           autoFocus
           placeholder="Write a message...."
           className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
+        />
+
+        <div className="absolute inset-y- right-0 flex py-1.5 pr-1.5">
+          <kbd className="inline-flex items-center rounded border bg-white border-gray-200 px-1 font-sense text-xs text-gray-400">
+            {isLoading ? (
+              <Loader2 className="w-3 h-3 animate-spin" />
+            ) : (
+              <CornerDownLeft className="w-3 h-3" />
+            )}
+          </kbd>
+        </div>
+
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600"
         />
       </div>
     </div>
